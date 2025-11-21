2290de56e4516c83e855ef1c49a4440ae4726676
xSoal <xsoal@mail.ru>
2025-11-21 17:39:20 +0200

    fix role adding admin user role

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -55,7 +55,7 @@ class UsersController extends Controller
 			}
 
 			$input['password'] = Hash::make($input['password']);
-
+            $user->role = 2;
             $user->fill($input);
 
             if( $user->save() ){
