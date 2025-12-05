9d80a29f63f8f699776b87a1b0a0474bc30c785b
xSoal <xsoal@mail.ru>
2025-12-05 13:40:32 +0200

    edit rename main page

--- resources/views/main_page/reference_information.blade.php
+++ resources/views/main_page/reference_information.blade.php
@@ -11,7 +11,7 @@
   <div class="mainImage__content">
     <div class="container">
       <div class="container-inner">
-        <h2 class="h2">Довідкова інформація</h2>
+        <h2 class="h2">Фінансові інструменти підтримки</h2>
       </div>
     </div>
   </div>
