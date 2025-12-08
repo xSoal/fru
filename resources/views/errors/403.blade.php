eeb0dc8de67228749cdd6b8434e1fddd6f11800f
xSoal <xsoal@mail.ru>
2025-12-08 11:58:56 +0200

    add custom errors page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.error')
+
+<div class="container">
+    <div class="container-inner">
+        <section class="ramstein">
+            <div class="">
+                <br><br><br>
+                <h1 style="text-align: center;">Доступ заборонений</h1>
+                <p style="text-align: center;">code: 403</p>
+            </div>
+        </section>
+
+    </div>
+
+</div>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.error')
+
+<div class="container">
+    <div class="container-inner">
+        <section class="ramstein">
+            <div class="">
+                <br><br><br>
+                <h1 style="text-align: center;">Помилка серверу</h1>
+                <p style="text-align: center;">code: 500</p>
+            </div>
+        </section>
+
+    </div>
+
+</div>
