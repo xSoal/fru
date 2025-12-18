0ba0737cb080c35791231c6eed7959fda1fc69ea
xSoal <xsoal@mail.ru>
2025-12-18 13:14:52 +0200

    fix bug password cabinets

--- app/Http/Controllers/CompanyAdmin/CompanyAdminController.php
+++ app/Http/Controllers/CompanyAdmin/CompanyAdminController.php
@@ -361,7 +361,7 @@ class CompanyAdminController extends Controller
         $updateData['web_page'] = $request->web_page;
         $updateData['contact_person'] = $request->contact_person;
 
-        if($request->has('password') && $request->has('new_password')){
+        if($request->password && $request->new_password === $request->new_password_confirm){
             $updateData['password'] = Hash::make($request->new_password);
         }
 
